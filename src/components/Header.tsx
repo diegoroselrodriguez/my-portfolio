eaac0bb9968a5ebef91a5947b8c0eef3e7b59022
Diego Rosel Rodríguez <diego.rosel89@gmail.com>
2025-11-20 10:35:26 +0100

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,7 +7,7 @@ export default function Header() {
         {/* Avatar con imagen real */}
         <div className="avatar">
           <img
-            src="/images/diego-perfil.jpg"
+            src="/my-portfolio/images/diego-perfil.jpg"
             alt="Foto de Diego Rodríguez"
             className="avatar-img"
           />
